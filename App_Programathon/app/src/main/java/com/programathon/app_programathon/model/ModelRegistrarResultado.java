c1e0d4fc744d97b9af63b3d833ab2094100d7147
Suzy12 <37936067+Suzy12@users.noreply.github.com>
2019-10-26 16:17:42 -0600

    Parte  Req 5

---
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/model/ModelRegistrarResultado.java
@@ -0,0 +1,13 @@
+package com.programathon.app_programathon.model;
+
+public class ModelRegistrarResultado {
+
+    private String comunicacion;
+    private String motoraGruesa;
+    private String motoraFina;
+    private String resProblemas;
+    private String socioInd;
+
+
+
+}
