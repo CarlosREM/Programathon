53a41fe00b8fb0fbcad9416466aaf33edbedd2c0
Suzy12 <37936067+Suzy12@users.noreply.github.com>
2019-10-26 14:49:33 -0600

    Menu y vetana Registrar

--- App_Programathon/app/src/main/java/com/programathon/app_programathon/activities/MenuNormalUserActivity.java
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/activities/MenuNormalUserActivity.java
@@ -1,8 +1,11 @@
 package com.programathon.app_programathon.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.programathon.app_programathon.R;
 
@@ -13,4 +16,27 @@ public class MenuNormalUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_normal_user);
     }
+
+    public void salirMensaje(View view){
+        DialogInterface.OnClickListener dialogInterface = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        finish();
+                        break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            break;
+
+                }
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Desea salir de la aplicación?")
+                .setPositiveButton("Aceptar", dialogInterface)
+                .setNegativeButton("Cancelar", dialogInterface).show();
+    }
+
+
 }
---
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/activities/RegistrarResultados.java
@@ -0,0 +1,15 @@
+package com.programathon.app_programathon.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.programathon.app_programathon.R;
+
+public class RegistrarResultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_resultados);
+    }
+}
