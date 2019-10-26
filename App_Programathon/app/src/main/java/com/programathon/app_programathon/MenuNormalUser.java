163aa6d86fbd3b7b2c93892e2597c41130939283
Suzy12 <37936067+Suzy12@users.noreply.github.com>
2019-10-26 11:55:32 -0600

    Menu User Normal

---
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/MenuNormalUser.java
@@ -0,0 +1,14 @@
+package com.programathon.app_programathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuNormalUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_normal_user);
+    }
+}
---
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/model/LogIn.java
@@ -0,0 +1,12 @@
+package com.programathon.app_programathon.model;
+
+public class LogIn {
+
+    public String userDNI;
+    public String password;
+
+    public LogIn(String user, String pw){
+        this.userDNI = user;
+        this.password = pw;
+    }
+}
