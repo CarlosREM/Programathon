120832b5c49742eed483851faff75d815a46d466
Suzy12 <37936067+Suzy12@users.noreply.github.com>
2019-10-27 03:53:50 -0600

    Arreglo testcalculadtor

--- App_Programathon/app/src/main/java/com/programathon/app_programathon/activities/CicloDesarrolloIntegral.java
+++ App_Programathon/app/src/main/java/com/programathon/app_programathon/activities/CicloDesarrolloIntegral.java
@@ -2,14 +2,32 @@ package com.programathon.app_programathon.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 import com.programathon.app_programathon.R;
+import com.programathon.app_programathon.globalconfig.ConfigConstants;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CicloDesarrolloIntegral extends AppCompatActivity {
 
@@ -17,6 +35,7 @@ public class CicloDesarrolloIntegral extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ciclo_desarrollo_integral);
+
 
         TextView txtName = findViewById(R.id.CicloDesarrollo_txtName),
                  txtGender = findViewById(R.id.CicloDesarrollo_txtGender),
@@ -49,4 +68,5 @@ public class CicloDesarrolloIntegral extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
 }
